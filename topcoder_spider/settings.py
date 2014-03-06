2f3fb99074586485af42b85ebf51ec80b4bb8f80
Developer <alert@AlertVM.(none)>
2014-03-06 14:17:14 +0800

    scrapy topcoder spider initialization

---
+++ topcoder_spider/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for topcoder_spider project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'topcoder_spider'
+
+SPIDER_MODULES = ['topcoder_spider.spiders']
+NEWSPIDER_MODULE = 'topcoder_spider.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'topcoder_spider (+http://www.yourdomain.com)'
---
+++ topcoder_spider/spiders/constants.py
@@ -0,0 +1,8 @@
+domain = 'topcoder.com'
+index_url = 'http://community.topcoder.com/'
+class Login:
+	login_url = 'http://community.topcoder.com/tc?module=Login'
+	formdata = {'username':'Andimeo', 'password':'19885245201314'}
+	formname = 'frmLogin'
+	
+match_list_url = 'http://community.topcoder.com/tc?module=MatchList&sc=&sd=&nr=200&sr=%d'
